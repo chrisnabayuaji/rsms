32802c0408fc26d1944898a2acead921ae7c57d4
chrisnabayuaji <chrisnabayuaji@gmail.com>
2025-04-15 06:07:58 +0700

    Add db_helper with CRUD operations and integrate into autoload configuration

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url','ignite','cookie','file');
+$autoload['helper'] = array('url','ignite','cookie','file','db');
 
 /*
 | -------------------------------------------------------------------
