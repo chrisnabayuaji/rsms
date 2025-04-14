3bba1b8860a2da6c3ee64587d367f997217b7dab
chrisnabayuaji <chrisnabayuaji@gmail.com>
2025-04-15 05:55:27 +0700

    peding

---
+++ application/modules/pelayanan/controllers/Pelayanan.php
@@ -0,0 +1,185 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Pelayanan extends MY_Controller
+{
+
+  var $menu_id, $menu, $cookie;
+
+  function __construct()
+  {
+    parent::__construct();
+
+    $this->load->model(array(
+      'config/m_config',
+      'm_pelayanan'
+    ));
+
+    $this->menu_id = '99.02';
+    $this->menu = $this->m_config->get_menu($this->menu_id);
+    if ($this->menu == null) redirect(site_url() . '/front/error_403');
+
+    //cookie 
+    $this->cookie = get_cookie_menu($this->menu_id);
+    if ($this->cookie['search'] == null) $this->cookie['search'] = array('term' => '');
+    if ($this->cookie['order'] == null) $this->cookie['order'] = array('field' => 'pelayanan_name', 'type' => 'asc');
+    if ($this->cookie['per_page'] == null) $this->cookie['per_page'] = 10;
+    if ($this->cookie['cur_page'] == null) 0;
+  }
+
+  public function index()
+  {
+    authorize($this->menu, '_read');
+    //cookie
+    $this->cookie['cur_page'] = $this->uri->segment(3, 0);
+    $this->cookie['total_rows'] = $this->m_pelayanan->all_rows($this->cookie);
+    set_cookie_menu($this->menu_id, $this->cookie);
+    //main data
+    $data['menu'] = $this->menu;
+    $data['cookie'] = $this->cookie;
+    $data['main'] = $this->m_pelayanan->list_data($this->cookie);
+    $data['pagination_info'] = pagination_info(count($data['main']), $this->cookie);
+    //set pagination
+    set_pagination($this->menu, $this->cookie);
+    //render
+    $this->render('index', $data);
+  }
+
+  public function form($id = null)
+  {
+    ($id == null) ? authorize($this->menu, '_create') : authorize($this->menu, '_update');
+    if ($id == null) {
+      create_log(2, $this->menu['menu_name']);
+      $data['main'] = null;
+    } else {
+      create_log(3, $this->menu['menu_name']);
+      $data['main'] = $this->m_pelayanan->by_field('pelayanan_id', $id);
+    }
+    $data['id'] = $id;
+    $data['menu'] = $this->menu;
+    $this->render('form', $data);
+  }
+
+  public function save($id = null)
+  {
+    ($id == null) ? authorize($this->menu, '_create') : authorize($this->menu, '_update');
+    $data = html_escape($data = $this->input->post(null, true));
+    if (!isset($data['is_active'])) {
+      $data['is_active'] = 0;
+    }
+    $cek = $this->m_pelayanan->by_field('pelayanan_name', $data['pelayanan_name']);
+    if ($id == null) {
+      if ($cek != null) {
+        $this->session->set_flashdata('flash_error', 'Nama sudah ada di sistem.');
+        redirect(site_url() . '/' .  $this->menu['controller']  . '/form/');
+      }
+      $data['pelayanan_id'] = $this->uuid->v4();
+      $this->m_pelayanan->save($data, $id);
+      create_log(2, $this->menu['menu_name']);
+      $this->session->set_flashdata('flash_success', 'Data berhasil ditambahkan.');
+    } else {
+      if ($data['old'] != $data['pelayanan_name'] && $cek != null) {
+        $this->session->set_flashdata('flash_error', 'Nama sudah ada di sistem.');
+        redirect(site_url() . '/' . $this->menu['controller'] . '/form/' . $id);
+      }
+      unset($data['old']);
+      $this->m_pelayanan->save($data, $id);
+      create_log(3, $this->menu['menu_name']);
+      $this->session->set_flashdata('flash_success', 'Data berhasil diubah.');
+    }
+    redirect(site_url() . '/' . $this->menu['controller'] . '/' . $this->menu['url'] . '/' . $this->cookie['cur_page']);
+  }
+
+  public function delete($id = null)
+  {
+    ($id == null) ? authorize($this->menu, '_create') : authorize($this->menu, '_update');
+    $this->m_pelayanan->delete($id);
+    create_log(4, $this->menu['menu_name']);
+    $this->session->set_flashdata('flash_success', 'Data berhasil dihapus.');
+    redirect(site_url() . '/' . $this->menu['controller'] . '/' . $this->menu['url'] . '/' . $this->cookie['cur_page']);
+  }
+
+  public function status($type = null, $id = null)
+  {
+    authorize($this->menu, '_update');
+    if ($type == 'enable') {
+      $this->m_pelayanan->update($id, array('is_active' => 1));
+    } else {
+      $this->m_pelayanan->update($id, array('is_active' => 0));
+    }
+    create_log(3, $this->this->menu['menu_name']);
+    redirect(site_url() . '/' . $this->menu['controller'] . '/' . $this->menu['url'] . '/' . $this->cookie['cur_page']);
+  }
+
+  public function multiple($type = null)
+  {
+    $data = $this->input->post(null, true);
+    if (isset($data['checkitem'])) {
+      foreach ($data['checkitem'] as $key) {
+        switch ($type) {
+          case 'delete':
+            authorize($this->menu, '_delete');
+            $this->m_pelayanan->delete($key);
+            $flash = 'Data berhasil dihapus.';
+            $t = 4;
+            break;
+
+          case 'enable':
+            authorize($this->menu, '_update');
+            $this->m_pelayanan->update($key, array('is_active' => 1));
+            $flash = 'Data berhasil diaktifkan.';
+            $t = 3;
+            break;
+
+          case 'disable':
+            authorize($this->menu, '_update');
+            $this->m_pelayanan->update($key, array('is_active' => 0));
+            $flash = 'Data berhasil dinonaktifkan.';
+            $t = 3;
+            break;
+        }
+      }
+    }
+    create_log($t, $this->menu['menu_name']);
+    $this->session->set_flashdata('flash_success', $flash);
+    redirect(site_url() . '/' . $this->menu['controller'] . '/' . $this->menu['url'] . '/' . $this->cookie['cur_page']);
+  }
+
+  public function authorization($id = null)
+  {
+    ($id == null) ? authorize($this->menu, '_create') : authorize($this->menu, '_update');
+    if ($id == null) {
+      create_log(2, $this->menu['menu_name']);
+      $data['main'] = null;
+    } else {
+      create_log(3, $this->menu['menu_name']);
+      $data['main'] = $this->m_pelayanan->by_field('pelayanan_id', $id);
+    }
+    $data['id'] = $id;
+    $data['menu'] = $this->menu;
+    $data['menu_list'] = $this->m_pelayanan->menu_list($id);
+    $this->render('authorization', $data);
+  }
+
+  public function authorization_save($id)
+  {
+    $data = html_escape($this->input->post());
+    $this->m_pelayanan->authorization_save($data, $id);
+    $this->session->set_flashdata('flash_success', "Data berhasil disimpan");
+    redirect(site_url() . '/' . $this->menu['controller'] . '/' . $this->menu['url'] . '/' . $this->cookie['cur_page']);
+  }
+
+  public function ajax($type = null, $id = null)
+  {
+    if ($type == 'check_id') {
+      $data = $this->input->post();
+      $cek = $this->m_pelayanan->by_field('pelayanan_name', $data['pelayanan_name']);
+      $old = $this->m_pelayanan->by_field('pelayanan_id', $id);
+      if ($id == null) {
+        echo ($cek != null) ? 'false' : 'true';
+      } else {
+        echo ($cek != null) ? (($cek['pelayanan_name'] == $old['pelayanan_name']) ? 'true' : 'false') : 'true';
+      }
+    }
+  }
+}
---
+++ application/modules/pelayanan/models/M_pelayanan.php
@@ -0,0 +1,149 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class M_pelayanan extends CI_Model
+{
+
+  public function where($cookie)
+  {
+    $where = "WHERE a.is_deleted = 0 ";
+    if (@$cookie['search']['term'] != '') {
+      $where .= "AND a.pelayanan_name LIKE '%" . $this->db->escape_like_str($cookie['search']['term']) . "%' ";
+    }
+    return $where;
+  }
+
+  public function list_data($cookie)
+  {
+    $where = $this->where($cookie);
+    $sql = "SELECT * FROM pelayanan a 
+      $where
+      ORDER BY "
+      . $cookie['order']['field'] . " " . $cookie['order']['type'] .
+      " LIMIT " . $cookie['cur_page'] . "," . $cookie['per_page'];
+    $query = $this->db->query($sql);
+    return $query->result_array();
+  }
+
+  public function all_data()
+  {
+    $where = "WHERE a.is_deleted = 0 ";
+
+    $sql = "SELECT * FROM pelayanan a $where ORDER BY created_at";
+    $query = $this->db->query($sql);
+    return $query->result_array();
+  }
+
+  public function all_rows($cookie)
+  {
+    $where = $this->where($cookie);
+
+    $sql = "SELECT COUNT(1) as total FROM pelayanan a $where";
+    $query = $this->db->query($sql);
+    return $query->row_array()['total'];
+  }
+
+  function by_field($field, $val)
+  {
+    $sql = "SELECT * FROM pelayanan WHERE $field = ?";
+    $query = $this->db->query($sql, array($val));
+    $row = $query->row_array();
+    return $row;
+  }
+
+  public function save($data, $id = null)
+  {
+    if ($id == null) {
+      $data['created_at'] = date('Y-m-d H:i:s');
+      $data['created_by'] = $this->session->userdata('user_fullname');
+      $this->db->insert('pelayanan', $data);
+    } else {
+      $data['updated_at'] = date('Y-m-d H:i:s');
+      $data['updated_by'] = $this->session->userdata('user_fullname');
+      $this->db->where('pelayanan_id', $id)->update('pelayanan', $data);
+    }
+  }
+
+  public function update($id, $data)
+  {
+    $data['updated_at'] = date('Y-m-d H:i:s');
+    $data['updated_by'] = $this->session->userdata('fullname');
+    $this->db->where('pelayanan_id', $id)->update('pelayanan', $data);
+  }
+
+  public function delete($id, $permanent = true)
+  {
+    if ($permanent) {
+      $this->db->where('pelayanan_id', $id)->delete('pelayanan');
+    } else {
+      $data['is_deleted'] = 1;
+      $data['updated_at'] = date('Y-m-d H:i:s');
+      $data['updated_by'] = $this->session->userdata('user_fullname');
+      $this->db->where('pelayanan_id', $id)->update('pelayanan', $data);
+    }
+  }
+
+  public function menu_list($id)
+  {
+    return $this->db->query(
+      "SELECT 
+      a.menu_id, a.parent_id, a.menu_name, 
+      a.is_read, a.is_create, a.is_update, a.is_delete, a.is_report,
+      b._read, b._create, b._update, b._delete, b._report
+    FROM menu a
+    LEFT JOIN pelayanan_menu b ON b.menu_id = a.menu_id AND b.pelayanan_id = '$id'
+    ORDER BY a.menu_id"
+    )->result_array();
+  }
+
+  public function authorization_save($data, $id)
+  {
+    $this->db->where('pelayanan_id', $id)->delete('pelayanan_menu');
+    //read
+    foreach (@$data['_read'] as $k => $v) {
+      $this->db->insert('pelayanan_menu', array(
+        'menu_id' => $v,
+        'pelayanan_id' => $id,
+        '_read' => 1,
+        'created_at' => date('Y-m-d H:i:s'),
+        'created_by' => $this->session->userdata('user_fullname'),
+      ));
+    }
+
+    //create
+    foreach (@$data['_create'] as $k => $v) {
+      $this->db->where(array('menu_id' => $v, 'pelayanan_id' => $id))->update('pelayanan_menu', array(
+        '_create' => 1,
+        'created_at' => date('Y-m-d H:i:s'),
+        'created_by' => $this->session->userdata('user_fullname'),
+      ));
+    }
+
+    //update
+    foreach (@$data['_update'] as $k => $v) {
+      $this->db->where(array('menu_id' => $v, 'pelayanan_id' => $id))->update('pelayanan_menu', array(
+        '_update' => 1,
+        'created_at' => date('Y-m-d H:i:s'),
+        'created_by' => $this->session->userdata('user_fullname'),
+      ));
+    }
+
+    //delete
+    foreach (@$data['_delete'] as $k => $v) {
+      $this->db->where(array('menu_id' => $v, 'pelayanan_id' => $id))->update('pelayanan_menu', array(
+        '_delete' => 1,
+        'created_at' => date('Y-m-d H:i:s'),
+        'created_by' => $this->session->userdata('user_fullname'),
+      ));
+    }
+
+    //report
+    foreach (@$data['_report'] as $k => $v) {
+      $this->db->where(array('menu_id' => $v, 'pelayanan_id' => $id))->update('pelayanan_menu', array(
+        '_report' => 1,
+        'created_at' => date('Y-m-d H:i:s'),
+        'created_by' => $this->session->userdata('user_fullname'),
+      ));
+    }
+  }
+}
